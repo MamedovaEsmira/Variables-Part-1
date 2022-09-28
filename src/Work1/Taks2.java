a74fa6d94b21d09ced9c08dc5bd3e6a724d0f8c2
mamedovaesmira <mamedovaesmira88@gmail.com>
2022-09-28 18:07:40 +0300

    ДЗ Сделано

--- src/Work1/Taks2.java
+++ src/Work1/Taks2.java
@@ -1,4 +1,25 @@
 package Work1;
 
 public class Taks2 {
-}
+    public static void main(String[] args) {
+        //Задача4
+        System.out.println("Задача4");
+        var friend =19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        //Задача5
+        System.out.println("Задача5");
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+    }
+    }
+
